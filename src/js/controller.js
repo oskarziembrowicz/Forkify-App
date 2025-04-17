e834649e3034ade9d2953fd63f8f826003bd5c6e
oskarziembrowicz <rakson2002@gmail.com>
2025-04-17 17:20:25 +0200

    Adding error handling

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,8 +4,6 @@ import 'regenerator-runtime/runtime';
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 
-const recipeContainer = document.querySelector('.recipe');
-
 ///////////////////////////////////////
 
 const controlRecipes = async function () {
@@ -22,13 +20,15 @@ const controlRecipes = async function () {
     // 2. Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
 
 // window.addEventListener('hashchange', controlRecipes);
 // window.addEventListener('load', controlRecipes);
